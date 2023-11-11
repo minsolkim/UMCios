878bd4ab16fa7efd077011c2d11b5e0b64a01d49
minsolkim <94881886+minsolkim@users.noreply.github.com>
2023-11-11 19:34:54 +0900

    카카오로그인
    카카오로그인 연결

---
+++ KakaoLogin/KakaoLogin/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  KakaoLogin
+//
+//  Created by 김민솔 on 2023/11/10.
+//
+
+import UIKit
+import KakaoSDKUser
+import KakaoSDKAuth
+
+class ViewController: UIViewController {
+    
+    var email: String?
+    var accessToken: String?
+    var name: String?
+
+    @IBOutlet weak var kakaologin: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        kakaologin.layer.cornerRadius = 10
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func loginWithKakaoTalk(_ sender: Any) {
+        var oauthToken: OAuthToken?
+
+        // isKakaoTalkLoginAvailable() : 카톡 설치 되어있으면 true
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            
+            // 카톡 설치되어있으면 -> 카톡으로 로그인
+            UserApi.shared.loginWithKakaoTalk { (token, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("카카오 톡으로 로그인 성공")
+                    
+                    oauthToken = token
+                    // 로그인 관련 메소드 추가
+                }
+            }
+        } else { // 카톡 없으면 -> 계정으로 로그인
+            UserApi.shared.loginWithKakaoAccount { (token, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("카카오 계정으로 로그인 성공")
+                    
+                    oauthToken = token
+                    // 관련 메소드 추가
+                }
+            }
+        }
+
+        // 사용자 정보 불러옴
+        UserApi.shared.me { [self] user, error in
+            if let error = error {
+                print(error)
+            } else {
+                
+                guard let token = oauthToken?.accessToken, let email = user?.kakaoAccount?.email,
+                      let name = user?.kakaoAccount?.profile?.nickname else {
+                    print("token/email/name is nil")
+                    return
+                }
+                
+                self.email = email
+                self.accessToken = token
+                self.name = name
+            }
+        }
+    }
+}
