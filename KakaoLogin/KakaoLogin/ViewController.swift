a3679692860b62118f4d877abb42cd558baa6580
minsolkim <94881886+minsolkim@users.noreply.github.com>
2023-11-15 22:28:17 +0900

    카카오로그인

--- KakaoLogin/KakaoLogin/ViewController.swift
+++ KakaoLogin/KakaoLogin/ViewController.swift
@@ -14,8 +14,11 @@ class ViewController: UIViewController {
     var email: String?
     var accessToken: String?
     var name: String?
-
+    
     @IBOutlet weak var kakaologin: UIButton!
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var infolabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         kakaologin.layer.cornerRadius = 10
@@ -24,7 +27,7 @@ class ViewController: UIViewController {
     
     @IBAction func loginWithKakaoTalk(_ sender: Any) {
         var oauthToken: OAuthToken?
-
+        
         // isKakaoTalkLoginAvailable() : 카톡 설치 되어있으면 true
         if (UserApi.isKakaoTalkLoginAvailable()) {
             
@@ -48,26 +51,52 @@ class ViewController: UIViewController {
                     
                     oauthToken = token
                     // 관련 메소드 추가
+                    self.setUserInfo()
                 }
-            }
-        }
-
-        // 사용자 정보 불러옴
-        UserApi.shared.me { [self] user, error in
-            if let error = error {
-                print(error)
-            } else {
-                
-                guard let token = oauthToken?.accessToken, let email = user?.kakaoAccount?.email,
-                      let name = user?.kakaoAccount?.profile?.nickname else {
-                    print("token/email/name is nil")
-                    return
-                }
-                
-                self.email = email
-                self.accessToken = token
-                self.name = name
             }
         }
     }
-}
+//        func setUserInfo() {
+//            // 사용자 정보 불러옴
+//            UserApi.shared.me { [self] user, error in
+//                if let error = error {
+//                    print(error)
+//                } else {
+//
+//                    guard let token = oauthToken?.accessToken, let email = user?.kakaoAccount?.email,
+//                          let name = user?.kakaoAccount?.profile?.nickname else {
+//                        print("token/email/name is nil")
+//                        return
+//                    }
+//
+//                    self.email = email
+//                    self.accessToken = token
+//                    self.name = name
+//                }
+//            }
+//        }
+        func setUserInfo() {
+            UserApi.shared.me() {(user, error) in
+                if let error = error {
+                    print(error)
+                }
+                else {
+                    print("me() success.")
+                    //do something
+                    _ = user
+                    if let nickname = user?.kakaoAccount?.profile?.nickname
+                        {
+                        // Update UI on the main thread
+                        DispatchQueue.main.async {
+                            self.infolabel.text = "닉네임: \(nickname)"
+                                    }
+                                }
+
+                    if let url = user?.kakaoAccount?.profile?.profileImageUrl,
+                       let data = try? Data(contentsOf: url) {
+                        self.profileImageView.image = UIImage(data: data)
+                    }
+                }
+            }
+        }
+    }
