806ad0961e5ce9d3417710da26a594d14cd07d03
minsolkim <123alsthf@naver.com>
2023-12-24 00:13:22 +0900

    banner

---
+++ DeliveryApp/DeliveryApp/ViewController/BannerCell.swift
@@ -0,0 +1,14 @@
+//
+//  BannerCell.swift
+//  DeliveryApp
+//
+//  Created by 김민솔 on 2023/12/23.
+//
+
+import UIKit
+
+class BannerCell: UICollectionViewCell {
+
+    @IBOutlet weak var imgView: UIImageView!
+    
+}
---
+++ DeliveryApp/DeliveryApp/ViewController/HomeViewController.swift
@@ -0,0 +1,169 @@
+//
+//  HomeViewController.swift
+//  DeliveryApp
+//
+//  Created by 김민솔 on 2023/12/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    @IBOutlet weak var ScrollV: UIScrollView!
+    
+    @IBOutlet weak var SearchBarV: UIView!
+    
+    @IBOutlet weak var SearchBarTextField: UIView!
+    
+    @IBOutlet weak var EventBar: UIView!
+    
+    @IBOutlet weak var ItemBar: UIView!
+    @IBOutlet weak var bannerCollectionV: UICollectionView!
+    private var timer : Timer?
+    private enum Color {
+        static var gradientColors = [
+          UIColor.systemBlue,
+          UIColor.systemBlue.withAlphaComponent(0.7),
+          UIColor.systemBlue.withAlphaComponent(0.4),
+          UIColor.systemGreen.withAlphaComponent(0.3),
+          UIColor.systemGreen.withAlphaComponent(0.7),
+          UIColor.systemGreen.withAlphaComponent(0.3),
+          UIColor.systemBlue.withAlphaComponent(0.4),
+          UIColor.systemBlue.withAlphaComponent(0.7),
+        ]
+      }
+      private enum Constants {
+        static let gradientLocation = [Int](0..<Color.gradientColors.count)
+          .map(Double.init)
+          .map { $0 / Double(Color.gradientColors.count) }
+          .map(NSNumber.init)
+          static let cornerRadius = 15.0
+          static let cornerWidth = 3.0
+      }
+    deinit {
+        self.timer?.invalidate()
+        self.timer = nil
+    }
+    var nowPage: Int = 0
+    let bannerImageArray: [UIImage] = [UIImage(named: "one.jpeg")!,UIImage(named: "two.jpeg")!,UIImage(named: "three.jpeg")!,UIImage(named: "four.jpeg")!,UIImage(named: "five.jpeg")!]
+    let canimation = CAAnimation()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        control()
+        bannerCollectionV.delegate = self
+        bannerCollectionV.dataSource = self
+        bannerTimer()
+    }
+    override func viewDidAppear(_ animated: Bool) {
+       super.viewDidAppear(animated)
+       
+       self.animateBorderGradation()
+     }
+    func control() {
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithTransparentBackground()
+        appearance.backgroundColor = UIColor(red: 0.16, green: 0.76, blue: 0.74, alpha: 1.0)
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navigationItem.standardAppearance = appearance
+        navigationItem.scrollEdgeAppearance = appearance
+        
+        //서치바 모양 뷰 만들기
+        SearchBarV.backgroundColor = UIColor(red: 0.16, green: 0.76, blue: 0.74, alpha: 1.0)
+        SearchBarV.layer.cornerRadius = 15
+        SearchBarV.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        SearchBarTextField.layer.cornerRadius = 2
+        SearchBarTextField.layer.shadowOffset = CGSize(width: 1, height: 1)
+        SearchBarTextField.layer.shadowOpacity = 0.3
+        SearchBarTextField.layer.shadowColor = UIColor.black.cgColor
+        //이벤트 모양 뷰 만들기
+        EventBar.layer.cornerRadius = 15
+        
+        //알뜸 한집 배달 모양 뷰 만들기
+        ItemBar.layer.cornerRadius = 15
+        ItemBar.layer.borderWidth = 3
+        
+
+    }
+    func bannerTimer() {
+    let _: Timer = Timer.scheduledTimer(withTimeInterval: 3, repeats: true) {
+        (Timer) in self.bannerMove()
+        }
+    }
+    //배너 움직임
+    func bannerMove() {
+        //현재 페이지가 마지막 페이지일 경우
+        if nowPage == bannerImageArray.count-1 {
+            bannerCollectionV.scrollToItem(at: NSIndexPath(item: 0, section: 0) as IndexPath, at: .right, animated: true)
+            nowPage = 0
+            return
+        }
+        //다음 페이지 전환
+        nowPage += 1
+        bannerCollectionV.scrollToItem(at: NSIndexPath(item: nowPage, section: 0) as IndexPath, at: .right, animated: true)
+    }
+    func animateBorderGradation() {
+        // 1. 경계선에만 색상을 넣기 위해서 CAShapeLayer 인스턴스 생성
+          let shape = CAShapeLayer()
+          shape.path = UIBezierPath(
+            roundedRect: self.EventBar.bounds.insetBy(dx: Constants.cornerWidth, dy: Constants.cornerWidth),
+            cornerRadius: self.EventBar.layer.cornerRadius
+          ).cgPath
+          shape.lineWidth = Constants.cornerWidth
+          shape.cornerRadius = Constants.cornerRadius
+          shape.strokeColor = UIColor.white.cgColor
+          shape.fillColor = UIColor.clear.cgColor
+        // 2. conic 그라데이션 효과를 주기 위해서 CAGradientLayer 인스턴스 생성 후 mask에 CAShapeLayer 대입
+          let gradient = CAGradientLayer()
+          gradient.frame = self.EventBar.bounds
+          gradient.type = .conic
+          gradient.colors = Color.gradientColors.map(\.cgColor) as [Any]
+          gradient.locations = Constants.gradientLocation
+          gradient.startPoint = CGPoint(x: 0.5, y: 0.5)
+          gradient.endPoint = CGPoint(x: 1, y: 1)
+          gradient.mask = shape
+          gradient.cornerRadius = Constants.cornerRadius
+          self.EventBar.layer.addSublayer(gradient)
+        // 3. 매 0.2초마다 마치 circular queue처럼 색상을 번갈아서 바뀌도록 구현
+          self.timer?.invalidate()
+          self.timer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { _ in
+            gradient.removeAnimation(forKey: "myAnimation")
+            let previous = Color.gradientColors.map(\.cgColor)
+            let last = Color.gradientColors.removeLast()
+            Color.gradientColors.insert(last, at: 0)
+            let lastColors = Color.gradientColors.map(\.cgColor)
+            
+            let colorsAnimation = CABasicAnimation(keyPath: "colors")
+            colorsAnimation.fromValue = previous
+            colorsAnimation.toValue = lastColors
+            colorsAnimation.repeatCount = 1
+            colorsAnimation.duration = 0.2
+            colorsAnimation.isRemovedOnCompletion = false
+            colorsAnimation.fillMode = .both
+            gradient.add(colorsAnimation, forKey: "myAnimation")
+          }
+    }
+
+}
+extension HomeViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return bannerImageArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = bannerCollectionV.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
+        cell.imgView.image = bannerImageArray[indexPath.row]
+        return cell
+    }
+}
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: bannerCollectionV.frame.size.width  , height:  bannerCollectionV.frame.height)
+    }
+    
+    //컬렉션뷰 감속 끝났을 때 현재 페이지 체크
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        nowPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+    }
+}
+
